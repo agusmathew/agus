86a403b3df4c24e8c2bf15600bfd05c2da05973f
Prasoon <prasoon.s@eraminfotech.in>
2018-09-20 18:20:08 +0530

    added static

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    'blog/static/',
 ]
 
 # Internationalization
@@ -124,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'blog/static/',
+]
